ff07f49335a8e94387557301fc7a690097aacf8f
sreerag-a-sasi <sreeragakhd2002@gmail.com>
2024-10-12 22:26:30 +0530

    12-10-24-10-26pm

--- HRM-client/src/Components/Reset-password.jsx
+++ HRM-client/src/Components/Reset-password.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import "./app.css"
+import "./Reset.css"
 
-export function App(props) {
+export function Resetpassword() {
   return (
     <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
         <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
@@ -50,7 +50,7 @@ export function App(props) {
             <div className="content">
 
                 <h2>Reset Password</h2>
-                <form id="reset-password" onsubmit="return handleReset(event)">
+                <form id="reset-password" onsubmit={handleReset()}>
                     <div><input type="password" placeholder="Enter The Password :" name="password" id="password" required /></div>
                     <div><button type="submit" id="btn">Submit</button> </div>
                 </form>
--- HRM-client/src/Components/Routing.jsx
+++ HRM-client/src/Components/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './Login';
 import GetDetails from './GetDetails';
+import { Resetpassword } from './Reset-password';
 
 
 export default function Routingapp() {
@@ -11,6 +12,7 @@ export default function Routingapp() {
         <Route path="/" exact element={<Login />} />
         <Route path="/GetDetails" exact element={<GetDetails />} />
         <Route path="/getUsersData" element={<GetDetails />} />
+        <Route path="/Reset-password" element={<Resetpassword />} />
       </Routes>
     </Router>
   );
